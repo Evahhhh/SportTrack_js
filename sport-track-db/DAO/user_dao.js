42858fae39cdba9167e0e7ef4742a22f2bcb7b10
Evahhhh <eve-anneoffredi@orange.fr>
2022-10-07 13:34:48 +0200

    update

--- sport-track-db/DAO/user_dao.js
+++ sport-track-db/DAO/user_dao.js
@@ -2,33 +2,40 @@ var db = require('../sqlite_connection.js');
 
 var UserDAO = function(){
     
-    this.insert = function(values){
-        db.run('INSERT INTO User (lName, fName, birthDate, gender, size, weight, email, password) VALUES (?,?,?,?,?,?,?,?)', values, call(console.log));
+    this.insert = function(values, callback){
+        db.run('INSERT INTO User (lName, fName, birthDate, gender, size, weight, email, password) VALUES (?,?,?,?,?,?,?,?)', values);
+        callback();
     };
     
     this.update = function(key, values){
-        db.run('UPDATE User SET lName = ?, fName = ?, birthDate = ?, gender = ? , size = ?, weight = ?, email = ?, password = ? WHERE id = ?', values, key, call(console.log));
+        db.run('UPDATE User SET lName = ?, fName = ?, birthDate = ?, gender = ? , size = ?, weight = ?, email = ?, password = ? WHERE id = ?', values, key);
     };
     
     this.delete = function(key){
-        db.run('DELETE FROM User WHERE id = ?', key, call(console.log));
+        db.run('DELETE FROM User WHERE id = ?', key);
     };
 
-    this.deleteAll = function(){
-        db.run('DELETE FROM User', call(console.log));
+    this.deleteAll = function(callback){
+        db.run('DELETE FROM User');
+        callback();   
     };
 
     this.findAll = function(){
-        db.all('SELECT * FROM User', call(console.log));
+        return new Promise((resolve, reject) => {
+            db.all('SELECT * FROM User', (err, row) => {
+                if (err) {
+                    reject(err); // optional: you might choose to swallow errors.
+                } else {
+                    resolve(row)
+                }
+            });
+        });
     };
     
     this.findByKey = function(key){
-        db.get('SELECT * FROM User WHERE id = ?', key, call(console.log));
+        return db.get('SELECT * FROM User WHERE id = ?', key);
     };
     
-    function call(callback){
-        setTimeout(()=>callback(1),10000);
-    }
 };
 
 var dao = new UserDAO();
--- test/sport-track-db-test.js
+++ test/sport-track-db-test.js
@@ -1,11 +1,25 @@
 var user_dao = require('../sport-track-db/sport-track-db').user_dao;
+
+
+
 var db = require('../sport-track-db/sport-track-db').db_connection;
 
-user_dao.deleteAll();
 
 var value = ['Doe', 'John', '1990-01-01', 'M', 1.80, 80,'doe.john@grimp.io','123456'];
-var value2 = ['Doe', 'Jane', '1990-01-01', 'F', 1.60, 60,'doe.jane@grimp.io','654321'];
-user_dao.insert(value);
-user_dao.insert(value2);
+var value2 = ['Done', 'Jane', '1990-01-01', 'F', 1.60, 60,'done.jane@grimp.io','654321'];
 
-user_dao.findAll();
+user_dao.deleteAll(() => { 
+    user_dao.insert(value, () => {
+        user_dao.insert(value2, () => {
+            user_dao
+                .findAll()
+                .then((res) => {
+                    console.log(res);
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+        });
+    });
+});
+
