67b92bde14147459f29155be96145eca7775c170
Evahhhh <eve-anneoffredi@orange.fr>
2022-10-12 17:40:55 +0200

    update

--- express_webapp/routes/users.js
+++ express_webapp/routes/users.js
@@ -1,11 +1,19 @@
 var express = require('express');
 var router = express.Router();
 var user_dao = require('../sport-track-db/sport-track-db').user_dao;
+
+//afficher tous les users de la bdd
 router.get('/', function(req, res, next) {
-  user_dao.findAll().then((rows) => {
-    res.render('users', { title: 'Users', users: rows });
-  }).catch((err) => {
-    console.log("ERROR= " +err);
-  });
+  console.log("hey")
+  res.render('users'),{ title: 'Inscription' };
 });
+
+router.post('/', function(req, res, next) {
+  // user_dao.insert(req.body).then((rows) => {
+  //   res.redirect('/users');
+  // }).catch((err) => {
+  //   console.log("ERROR= " +err);
+  // });
+});
+
 module.exports = router;
