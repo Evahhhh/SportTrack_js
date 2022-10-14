3bcac4cdbf3ff193324690fabdc8e5a68251905f
Evahhhh <eve-anneoffredi@orange.fr>
2022-10-14 10:05:52 +0200

    update

--- express_webapp/routes/index.js
+++ express_webapp/routes/index.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express", sessionId: req.session.idUser });
+  res.render("index", { title: "SportTrack", sessionId: req.session.idUser });
 });
 
 module.exports = router;
